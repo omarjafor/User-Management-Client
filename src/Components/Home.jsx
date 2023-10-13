a85a806da960930a2dc33245d78398e82b13a604
omarjafor <omarjaforchy@gmail.com>
2023-10-13 18:52:27 +0600

    user update start

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 
 const Home = () => {
-    
+    const [users, setUsers] = useState([]);
 
     useEffect( () => {
         fetch('http://localhost:5000/user')
