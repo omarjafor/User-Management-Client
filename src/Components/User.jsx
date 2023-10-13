8a7d60bfb032abb06da577f71433cfa3f8b008ad
omarjafor <omarjaforchy@gmail.com>
2023-10-13 12:29:09 +0600

    user delete

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -3,9 +3,21 @@ import { useLoaderData } from 'react-router-dom';
 const User = () => {
     const user = useLoaderData();
 
+    const handleDelete = ( _id ) => {
+        console.log(_id);
+    }
+
     return (
         <div>
             <h2> {user.length} </h2>
+            <div>
+                {
+                    user.map(usr => <p key={usr._id}> {usr.name} : {usr.email} <button
+                        onClick={() => handleDelete(usr._id)}
+                    >X</button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
